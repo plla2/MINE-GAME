c5eb83ce9b951d25bd49167ede9f17c1c92f765b
plla2 <jhbljs92@naver.com>
2023-09-11 00:14:48 +0900

    ⚡️ 전역 ThemeColors, shadow 설정

---
+++ src/styles/ThemeColors.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from 'styled-components';
+
+export const ThemeColors: DefaultTheme = {
+  color: {
+    cell_border: '#a7727d',
+    cell_open: '#F8EDE3',
+    cell_normal: '#DFD3C3',
+    red_color: '#F24C3D',
+    font_color: '#8b7e74',
+    board_border: '#eddbc7',
+    background: '#f9f5e7',
+    controller_bg: '#F1DEC9',
+    title_color: '#594545',
+    controller_border: '#eac7c7',
+    hover_color: '#ffe5ca',
+  },
+  shadow: {
+    box_shadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.3)',
+  },
+};
+
+export default ThemeColors;
