4029222e33a223d8ac967ce2f8cb66d0ad4878bc
plla2 <jhbljs92@naver.com>
2023-09-10 18:36:02 +0900

    💄 Styled-components 전역테마 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './styles/resetStyles.css';
+import './styles/resetStyles.scss';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store.ts';
 import PageLayout from './components/PageLayout/PageLayout.tsx';
+import GlobalStyles from './styles/GlobalStyles.ts';
+import { ThemeProvider } from 'styled-components';
+import ThemeColors from './styles/ThemeColors.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <PageLayout>
-      <App />
-    </PageLayout>
-  </Provider>,
+  <ThemeProvider theme={ThemeColors}>
+    <Provider store={store}>
+      <PageLayout>
+        <GlobalStyles />
+        <App />
+      </PageLayout>
+    </Provider>
+  </ThemeProvider>,
 );
